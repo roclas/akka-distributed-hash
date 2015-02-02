4972b36bdefff1f6ed1173de81bcaa111712731c
Carlos <carlopolo@gmail.com>
2015-02-02 14:47:26 +0100

    cleaning code

--- README.md
+++ README.md
@@ -6,22 +6,23 @@ How it works:
 
 1- start the first node of the cluster (on port 2551): sbt "run 2551"   
 
-2- start the second node of the cluster (on port 2552): sbt "run 2552"
+2- check the hash on the fist node through an http GET request (asking for the key "hello"): curl http://localhost:8551
+    (you'll see nothing; the hash is empty)
 
-3- check the hash on the fist node through an http GET request: curl http://localhost:8551/ping
-    (you'll see what the hash has)
+3- put elements into the hash through an http PUT request: curl -X PUT http://localhost:8551 --data "hello=123&world=456"
+    (this will insert some more data in that node's hash)
 
-4- check the hash on the fist node through an http GET request: curl http://localhost:8552/ping
-    (you'll see what the hash has)
-    
-5- modify the content of the first hash through an http PUT request: curl -X PUT http://localhost:8551/ping --data "mysecondparam=123&BBQ=456"
-    (this will insert some more data -hardcoded- in that node's hash)
+4- check the hash on the fist node through an http GET request: curl http://localhost:8552/hello
+    (you'll see what the hash has for "hello")
 
-6- check that the hash has been copied to the second (actually to all) node(s) of the cluster: curl http://localhost:8552/ping
+5- start the second node of the cluster (on port 2552): sbt "run 2552"
+
+6- check that the hash has been copied to the second (actually to all) node(s) of the cluster: curl http://localhost:8552/hello
 
 AND YOU ARE DONE!!
 
-*** http delete (Eg: " curl -X DELETE http://localhost:8551/ping --data 'hola' ") are supported
+*** http deletes (Eg: " curl -X DELETE http://localhost:8551 --data 'hello' ") are supported
+*** try now to delete something from one of the nodes and check that it's been deleted in the other nodes
 
 
 //TODO: the second node has to sincronize on initialization
--- src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
+++ src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
@@ -1,11 +1,15 @@
 package sample.cluster.simple
 
-import java.util
+
+import java.io.{ObjectOutputStream, ByteArrayOutputStream}
 
 import akka.actor.{Actor, ActorLogging}
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent._
 import akka.cluster.MemberStatus.Up
+import java.util
+import java.security.MessageDigest
+import scala.collection.JavaConversions._
 import spray.can.Http
 import spray.http._
 
@@ -23,7 +27,7 @@ class SimpleClusterListener extends Actor with ActorLogging {
   override def postStop(): Unit = cluster.unsubscribe(self)
   
   val hash=new util.HashMap[String,String]()
-  hash.put("hola","hola")
+  val md5s=List()
 
   def getParamsMap(s: String) = {
     s.split('&') map { str=>
@@ -32,31 +36,39 @@ class SimpleClusterListener extends Actor with ActorLogging {
     } toMap
   }
 
+  def md5(m: java.util.HashMap[_,_]) = {
+    val baos = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(baos)
+    oos.writeObject(m)
+    oos.close()
+    val md = MessageDigest.getInstance("MD5")
+    md.update(baos.toByteArray());
+    md.digest()
+  }
+
   def receive = {
-    case s:String=>
-      log.info("Receiving String: {}", s)
-      sender ! hash.toString
     case MemberUp(member) =>
+      context.actorSelection(member.address+"/user/clusterListener") ! syncronize(hash)
       log.info("Member is Up: {}", member.address)
       log.info("Current members:{}",cluster.state.members.filter(_.status == Up))
-
     case UnreachableMember(member) =>
       log.info("Member detected as unreachable: {}", member)
     case MemberRemoved(member, previousStatus) =>
       log.info("Member is Removed: {} after {}", member.address, previousStatus)
       log.info("Current members:{}",cluster.state.members.filter(_.status == Up))
     case _: MemberEvent => // ignore
-
-
+    case syncronize(map)=>
+      log.info("Syncronizing {} and {}",md5(hash),md5(map))
+      for((k,v)<-map){ hash.put(k,v) }
+      //for((k,v)<-hash){ if(map.get(k)==null)hash.remove(k) }
     case delete(data:String)=>
       log.info("deleting{}",data)
       hash.remove(data)
-      
     case put(k:String,v:String)=>
       log.info("putting {} {}",k,v)
       hash.put(k,v)
       
-    case req@HttpRequest(HttpMethods.PUT, Uri.Path("/ping"), headers, entity, protocol) =>{
+    case req@HttpRequest(HttpMethods.PUT, _, _, _, _) =>{
       val data=req.entity.asString(HttpCharsets.`UTF-8`)
       val hashdata=getParamsMap(data)
       for((k,v)<-hashdata){
@@ -64,12 +76,11 @@ class SimpleClusterListener extends Actor with ActorLogging {
         hash.put(k,v)
         for (m<-cluster.state.members.filter(_.status == Up)){
           context.actorSelection(m.address+"/user/clusterListener") ! put(k,v)
-          log.info("\n\n\nsending hash to"+m.address)
         }
       }
       sender ! HttpResponse(entity = hash.toString)
     }
-    case req@HttpRequest(HttpMethods.DELETE, Uri.Path("/ping"), headers, entity, protocol) =>{
+    case req@HttpRequest(HttpMethods.DELETE, _, _, _, _) =>{
       val data=req.entity.asString(HttpCharsets.`UTF-8`)
       log.info("Receiving DELETE request query param: {} ", data)
       hash.remove(data)
@@ -78,15 +89,14 @@ class SimpleClusterListener extends Actor with ActorLogging {
         context.actorSelection(m.address+"/user/clusterListener") ! delete(data)
       }
     }
-    case req@HttpRequest(HttpMethods.GET, Uri.Path("/ping"), headers, entity, protocol) =>
-      val somekey=req.uri.query.get("key")
-      val key=somekey match {
-        case None=>" "
-        case null=>" "
-        case Some(name)=>name
+    case req@HttpRequest(HttpMethods.GET, _, _, _, _) =>
+      var response=""
+      if(req.uri.path!=null)if(req.uri.path.toString.length>0){
+        val k= req.uri.path.toString.replace("/", "")
+        if(hash.get(k)!=null)response=hash.get(k)
       }
-      log.info("receiving get {}",key)
-      sender ! HttpResponse(entity = hash.get(key.toString))
+      log.info("sending response {} ",response)
+      sender ! HttpResponse(entity = response)
     case r:HttpRequest =>
       sender ! HttpResponse(entity = "this page doesn't exist")
     case c:akka.io.Tcp.Connected =>
